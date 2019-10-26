0b66829eb28d491617f5c05bd6e5c13255619bf3
Trung <khactrung01203375867@gmail.com>
2019-10-26 15:59:16 +0700

    DA_Laravel
    ok

---
+++ thefirst_project/resources/views/partials/topbar.blade.php
@@ -0,0 +1,21 @@
+<div class="navbar-custom">
+                <div class="container-fluid">
+                  
+
+                    <!-- LOGO -->
+                    <div class="logo-box">
+                        <a href="index.html" class="logo text-center">
+                            <span class="logo-lg">
+                                <img src="{{ asset ('assets/images/logo-dark.png') }}" alt="" height="26">
+                                <!-- <span class="logo-lg-text-dark">Upvex</span> -->
+                            </span>
+                            <span class="logo-sm">
+                                <!-- <span class="logo-sm-text-dark">X</span> -->
+                                <img src="{{ asset ('assets/images/logo-sm.png') }}" alt="" height="28">
+                            </span>
+                        </a>
+                    </div>
+                 
+                    <div class="clearfix"></div>
+                </div>
+            </div>
